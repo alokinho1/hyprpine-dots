5cb1f47748c91499076ce65e47bd7b5a4139943c
alokinho1 <89alokinho@gmail.com>
2025-03-13 10:58:12 +0530

    Add files via upload

---
+++ .config/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,26 @@
+local options = {
+  ensure_installed = {
+    "bash",
+    "c",
+    "cpp",
+    "fish",
+    "lua",
+    "luadoc",
+    "markdown",
+    "printf",
+    "toml",
+    "vim",
+    "vimdoc",
+    "yaml",
+    "hyprlang",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
